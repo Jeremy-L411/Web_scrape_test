0c42ec1462002dcaca574b52be3383b2893fc3b5
Jeremy <36969209+Jeremy-L411@users.noreply.github.com>
2020-05-25 20:32:26 -0600

    Update web_scrape.py
    completely rewrote, able to find first two rows, unable to locate entire table

--- web_scrape.py
+++ web_scrape.py
@@ -1,24 +1,22 @@
 import bs4 as bs
 import requests
-import urllib.request
 
-# urllib does not work, SSL error
-# source = urllib.request.urlopen('https://www.repeaterbook.com/repeaters/TravelSearch.php?route=I-90&state_id=46')
-source = requests.get('https://www.repeaterbook.com/repeaters/TravelSearch.php?route=I-90&state_id=46')
-soup = bs.BeautifulSoup(source, 'xml.parser')
+url = "https://www.repeaterbook.com/repeaters/TravelSearch.php?route=I-90&state_id=46"
+r = requests.get(url)
 
+soup = bs.BeautifulSoup(r.content, "html.parser")
+table = soup.find_all('table')[2]
+rows = table.find_all('tr')
+row_list = []
+print(rows)
 
-table = soup.find('table')
-
-table_rows = table.find_all('tr')
-
-for tr in table_rows:
+for tr in rows:
     td = tr.find_all('td')
     row = [i.text for i in td]
-    print(row)
+    row_list.append(row)
 
-"""
-Attempting to get top of table "Frequency", "Offset", "Tone", "Call" on top
-and then loop through the frequencies in the table. It is the second table that I see 
-in the inspector, the first is the "On air, off air, Testing, unknown. 
+print(row_list)
+
+""" 
+Updated, First print row finds top and first line, unable to locate any other rows afterwords 
 """
